c62c0ee47e084e4d64c164b2401d431eccddee6d
suchenguo <65109015+guozhenduo@users.noreply.github.com>
2020-08-17 15:03:03 +0800

    Update main.h

--- main.h
+++ main.h
@@ -4,4 +4,9 @@
 #include "smain1.c"
 #include "csf.c"
 #include "chtime.c"
-
+extern int table[12];
+extern char table2[][7];
+extern char table3[][12];
+extern char table4[][12];
+extern char table5[][12];
+extern char table6[][12];
