2c06ee5805f596a7ad258088dffd6e2e4b27280e
suchenguo <65109015+guozhenduo@users.noreply.github.com>
2020-08-17 10:12:40 +0800

    Create smain1.c

---
+++ smain1.c
@@ -0,0 +1,96 @@
+// smain1.c: small_main1.c
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char table6[][12];
+
+int month_day[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+ 
+int * cal_dafy(int year,int day,int res[])
+{
+	int max = is_leap(year) ?  366 : 365,month = 1;
+	if (day > max) return 0;
+	month_day[1] =  is_leap(year) ? 29 : 28;
+	while (day > month_day[month - 1])
+	{
+		day -= month_day[month - 1];
+		month++;
+	}
+
+	res[0] = month, res[1] = day;
+
+	return res;
+}
+
+int dafy(void) // day after year
+{
+	int *u,year = 0,day = 0,arr[2] = {0,0};
+	printf("Enter the year and the day(format:YEAR->DAY): ");scanf("%d->%d",&year,&day);
+	u = cal_dafy(year,day,arr);
+	printf("%s %d(%d-%d) \n",table5[*(u + 0) - 1],*(u + 1),*(u + 0),*(u + 1));
+
+	return 0;
+}
+
+int interval_days(int start[],int end[])
+{
+    int count = 0,year_days = 0,i = 0;
+	if (start[2] > end[2]) count -= (start[2] > end[2]);
+	if (start[2] < end[2]) count += (end[2] - start[2]);
+	start[2] = end[2];
+
+	if (start[1] < end[1])
+	{
+		while (start[1] != end[1])
+		{
+		        i = start[1] - 1; // i is a month number
+			count += month_day[i];
+			start[1]++;
+	        }
+	}
+
+	if (start[1] > end[1])
+	{
+	        while (start[1] != end[1])
+		{
+		        i = start[1] - 1;
+			count -= month_day[i];
+			start[1]--;
+		}
+	}
+
+	if (start[0] < end[0])
+	{
+		while (start[0] != end[0])
+		{
+		        year_days = is_leap(start[0] + 1) ?366 : 365;
+			count += year_days;
+			start[0]++;
+		}
+	}
+
+	if (start[0] > end[0])
+	{
+	    while (start[0] != end[0])
+		{
+		        year_days = is_leap(start[0] - 1) ? 366 : 365;
+			count -= year_days;
+			start[0]--;
+		}
+	}
+	
+	return abs(count);
+}
+
+int idays(void)
+{
+    int start_date[3] = {0,0,0};int end_date[3] = {0,0,0};
+	printf("Enter the start date(format:YEAR-MONTH-DAY): ");
+	scanf("%d-%d-%d",&start_date[0],&start_date[1],&start_date[2]);
+	printf("Enter the end date(format:YEAR-MONTH-DAY): ");
+    scanf("%d-%d-%d",&end_date[0],&end_date[1],&end_date[2]);
+	printf("The two dates are %d days apart \n",interval_days(start_date,end_date));
+
+	return 0;
+}
+
